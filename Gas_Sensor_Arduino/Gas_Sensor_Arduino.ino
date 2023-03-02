0f53da964b7cdb85526ef529ff180f382a8a206a
SamuelACGarcia <123728485+SamuelACGarcia@users.noreply.github.com>
2023-03-02 14:14:03 +0100

    Update Gas_Sensor_Arduino.ino
    Replace with a code that works better

--- Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
+++ Gas_Sensor_Arduino/Gas_Sensor_Arduino.ino
@@ -1,39 +1,27 @@
 #include <MQ2.h>
 
-int analog_IN = 0; // This is our input pin
-// int digital_IN = D1; // This is our input pin
+int analog_IN = 2;  // This is our input pin (IO2)
 MQ2 mq2(analog_IN);
 
-float sensorValue;
-
-void setup()
-{
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // pinMode(analog_IN, INPUT);
+void setup() {
   Serial.begin(9600);
-
+  
   mq2.begin();
-  Serial.println("MQ2 warming up!");
-  delay(20000); // allow the MQ2 to warm up
+  delay(20000);
 }
 
-void loop()
-{
-  sensorValue = analogRead(analog_IN);
-  float *values = mq2.read(false);
-  // Serial.print("MQ2 Read CO : ");
-  Serial.println(values[0]);
-  // Serial.print("Digital Read : ");
-  // Serial.println(digitalRead(digital_IN));
-  Serial.print("Analogic Read : ");
-  Serial.println(sensorValue);
-  /*if (digitalRead(digital_IN) == LOW){
-    Serial.println("Seuil dépassé : Gaz detecté");
+void loop() {
+  
+  
+  float* values= mq2.read(true);
+//  float smokeLvl= mq2.readSmoke(); 
+//  
+//  Serial.print("MQ2 Read SMOKE : "); Serial.print(smokeLvl); 
+  Serial.print("|");
+
+  /*if (smokeLvl > 0.03){
+    Serial.println("Seuil dépassé : CO2 détecté");
   }*/
-  // while(Serial.available() > 0) {
-  //   Serial.write(Serial.read());
-  // }
-  // Serial.write(itoa(mq2.readCO(), cstr, 10));
-  // Serial.println();
-  delay(1000); // wait for a second
+  
+  delay(1000);                       // wait for a second
 }
